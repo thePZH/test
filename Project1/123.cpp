771a68b02a61d13290ea52a119fd3dfd8cf272e5
pzh <1057721283@qq.com>
2023-10-29 11:51:10 +0800

    3

---
+++ Project1/123.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+// 你是我的神
+void test()
+{
+	std::cout << "1" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "1" << std::endl;
+}
